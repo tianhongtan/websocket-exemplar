637969d4454d98a409ea8b1aad7cec8ecb34618d
Tim Tan <tim.tan@veeva.com>
2016-10-28 15:41:19 -0700

    Handle multitenancy with websockets.

--- app/main.js
+++ app/main.js
@@ -1,3 +1,11 @@
+var userId = localStorage.getItem("userId") || randomId();
+var messageCache;
+localStorage.setItem("userId", userId);
+
+function randomId() {
+    return Math.floor(Math.random() * 1e11);
+}
+
 
 var socket = io.connect(
     'http://localhost',
@@ -6,4 +14,58 @@ var socket = io.connect(
 
 socket.on("messages", function(data) {
     console.info(data);
+    messageCache = data;
+    render();
 });
+
+function render() {
+    var data = messageCache;
+    var html = data.sort(function(a, b) {
+        return a.ts - b.ts;
+    }).map(function(data, index) {
+        return (`
+        <form class="message" onsubmit="return likeMessage(messageCache[${index}])">
+            <div class='name'>
+                ${data.userName}
+            </div>
+            <a href=${data.content.link} class = 'message' target = blank>
+                ${data.content.text}
+            </a>
+            <div class="time">${moment(data.ts).fromNow()}</div>
+            <input type='submit' class="likes-count" value="${data.likedBy.length} Likes">
+        </form>
+
+        `)
+    }).join(" ");
+    document.getElementById("messages").innerHTML = html;
+
+}
+
+function likeMessage(message) {
+    var index = message.likedBy.indexOf(userId);
+    if (index < 0) {
+        message.likedBy.push(userId);
+    } else {
+        message.likedBy.splice(index, 1);
+    }
+
+    socket.emit("update-message", message);
+    render();
+    return false;
+}
+
+function addMessage(e) {
+    var payload = {
+        messageId:randomId(),
+        userName:document.getElementById("username").value,
+        content: {
+            text: document.getElementById("messages").value,
+            link: document.getElementById("linkAddress").value
+        },
+        likedBy: [],
+        ts: Date.now()
+
+    };
+    socket.emit("new-message", payload);
+    return false;
+}
--- server/main.js
+++ server/main.js
@@ -4,10 +4,55 @@ var server = require('http').Server(app); //instance of express wrapped in http
 var io = require('socket.io')(server); //invoke w/server
 
 app.use(express.static('app'));
+app.use('/bower_components', express.static('bower_components'));
+
+var messages = [{
+    userId:1,
+    messageId:10,
+    userName:"Asha Greyjoy",
+    content:{
+        text:"The stone tree of the Stonetrees.",
+        link:"http://awoiaf.westeros.org/index.php/House_Stonetree"
+    },
+    likedBy:[1],
+    ts:Date.now() - 10000
+},{
+    userId:2,
+    messageId:11,
+    userName:"Arya Stark",
+    content:{
+        text:"We'll come see this inn.",
+        link:"http://gameofthrones.wikia.com/wiki/Inn_at_the_Crossroads"
+    },
+    likedBy:[2,3],
+    ts:Date.now() - 100000
+},{
+    userId:3,
+    messageId:14,
+    userName:"Cersei Lannister",
+    content:{
+        text:"Her scheming forced this on me.",
+        link:"http://gameofthrones.wikia.com/wiki/Margaery_Tyrell"
+    },
+    likedBy:[],
+    ts:Date.now() - 1000000
+}];
 
 io.on('connection', function(socket) {
-    console.log("Something ocnnected to Socket.io");
-    socket.emit("messages", ["Hello", "Hi there", "How are you? "]);
+    console.log("Something connected to Socket.io");
+    socket.emit("messages", messages);
+    socket.on("new-message", function(data) {
+        messages.push(data);
+        io.sockets.emit("messages", messages); //emits to all sockets, not just the passed in socket.
+    });
+    socket.on("update-message", function(data) {
+        var message = messages.filter(function(message) {
+            return message.messageId === data.messageId;
+        })[0];
+
+        message.likedBy = data.likedBy;
+        io.sockets.emit("messages", messages); //emits to all sockets, not just the passed in socket.
+    });
 });
 
 server.listen(80);
